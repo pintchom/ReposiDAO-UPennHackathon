8eb643548ff3f0fa63e989ab265847f96dd26776
Max Pintchouk <maxpintchouk4321@gmail.com>
2024-02-17 00:20:48 -0500

    small stuff

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ def git_log():
 
     if cli_result.returncode == 0:
         result = cli_result.stdout
+        print(result)
         print("Output saved to git_shortlog_output.txt")
     else:
         print("Error executing command:", cli_result.stderr)
+
+    #splice commit id and email into firebase commit_count += 1
+    
+
     return result
 
+git_log()
 
