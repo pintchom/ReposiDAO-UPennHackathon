07cc6299fe054e0b60f88388e2fc4f380a85d63c
Max Pintchouk <maxpintchouk4321@gmail.com>
2024-02-17 00:24:21 -0500

    regex parse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import subprocess
+import re
+
 
 def git_log():
     command = "git log -1"
@@ -13,7 +15,16 @@ def git_log():
         print("Error executing command:", cli_result.stderr)
 
     #splice commit id and email into firebase commit_count += 1
-    
+    pattern = r"commit ([0-9a-fA-F]{40}).*?<([\w\.-]+@[\w\.-]+)>"
+    match = re.search(pattern, result, re.DOTALL)
+
+    if match:
+        commit_id = match.group(1)
+        email = match.group(2)
+        print(f"Commit ID: {commit_id}")
+        print(f"Email: {email}")
+    else:
+        print("No match found")
 
     return result
 
