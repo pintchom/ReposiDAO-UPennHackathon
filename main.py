43ffd2aaedd86753b73137ea07c3dc9d06d98472
Max Pintchouk <maxpintchouk4321@gmail.com>
2024-02-17 13:28:09 -0500

    firebase stores git info

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess
 import re
 import firebase_admin as fa 
-from google.cloud import firestore
+from firebase_admin import firestore as fs
 
 cred = fa.credentials.Certificate("reposadao-b831e-firebase-adminsdk-q252c-10034dc1e1.json")
 fa.initialize_app(cred)
@@ -21,15 +21,24 @@ def git_log():
     pattern = r"commit ([0-9a-fA-F]{40}).*?<([\w\.-]+@[\w\.-]+)>"
     match = re.search(pattern, result, re.DOTALL)
 
-    if match:
-        commit_id = match.group(1)
-        email = match.group(2)
-        #print(f"Commit ID: {commit_id}")
-        #print(f"Email: {email}")
-        db = firestore.Client()
-        contributor_ref = db.collection('contributors').document(email)
+    commit_id = match.group(1) #commit ID from git log 
+    email = match.group(2) # user's email of commit
+    db = fs.client()
+    contributor_ref = db.collection('contributors').document(email)
+    contributor_data = contributor_ref.get().to_dict() or {}
 
-    return result
+    contributor_data['email'] = email
+
+    if contributor_data.get('commit_ids'):
+        if commit_id != contributor_data['commit_ids'][-1]:
+            contributor_data['commit_ids'].append(commit_id)
+            contributor_data['contributions'] += 1
+    else:
+        contributor_data['commit_ids'] = [commit_id]
+        contributor_data['contributions'] = 1
+
+    contributor_ref.set(contributor_data)
+    return #result
 
 git_log()
 
