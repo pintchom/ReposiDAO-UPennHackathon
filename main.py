c76b0b51d9131f2056d313b1ac4746e75bca98ab
Max Pintchouk <maxpintchouk4321@gmail.com>
2024-02-17 00:15:09 -0500

    changed command to git log

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import subprocess
 
-command = "git shortlog -sn --all"
+command = "git log"
 result = subprocess.run(command, shell=True, text=True, capture_output=True)
 
 if result.returncode == 0:
-    with open("git_shortlog_output.txt", "w") as file:
+    with open("git_log_output.txt", "w") as file:
         file.write(result.stdout)
     print("Output saved to git_shortlog_output.txt")
 else:
