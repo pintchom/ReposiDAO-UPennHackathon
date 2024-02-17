d0c72369f6607d0349481d9c52b8ab7f32db8f9b
Max Pintchouk <maxpintchouk4321@gmail.com>
2024-02-17 00:19:15 -0500

    function dec + flask init

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import subprocess
 
-command = "git log"
-result = subprocess.run(command, shell=True, text=True, capture_output=True)
+def git_log():
+    command = "git log"
+    cli_result = subprocess.run(command, shell=True, text=True, capture_output=True)
+    result = ""
 
-if result.returncode == 0:
-    with open("git_log_output.txt", "w") as file:
-        file.write(result.stdout)
-    print("Output saved to git_shortlog_output.txt")
-else:
-    print("Error executing command:", result.stderr)
+    if cli_result.returncode == 0:
+        result = cli_result.stdout
+        print("Output saved to git_shortlog_output.txt")
+    else:
+        print("Error executing command:", cli_result.stderr)
+    return result
+
+
--- poo.py
+++
@@ -1,4 +0,0 @@
-def main():
-    print("poo")
-
-main()
