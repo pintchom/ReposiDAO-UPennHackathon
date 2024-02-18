e96bd47a8bdee18d379091092ce6bc18590c4327
Max Pintchouk <maxpintchouk4321@gmail.com>
2024-02-18 14:37:13 -0500

    more init stuff

---
+++ constants.py
@@ -0,0 +1,1 @@
+sepolia_endpoint = "https://sepolia.infura.io/v3/9a40be699fea4fcf9ba80ad8495a3eaf"
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import subprocess
 import re
-import firebase_admin as fa 
+import firebase_admin as fa
 from firebase_admin import firestore as fs
 
 cred = fa.credentials.Certificate("reposadao-b831e-firebase-adminsdk-q252c-10034dc1e1.json")
@@ -17,11 +17,10 @@ def git_log():
     else:
         print("Error executing command:", cli_result.stderr)
 
-    #splice commit id and email into firebase commit_count += 1
     pattern = r"commit ([0-9a-fA-F]{40}).*?<([\w\.-]+@[\w\.-]+)>"
     match = re.search(pattern, result, re.DOTALL)
 
-    commit_id = match.group(1) #commit ID from git log 
+    commit_id = match.group(1) #commit ID from git log
     email = match.group(2) # user's email of commit
     db = fs.client()
     contributor_ref = db.collection('contributors').document(email)
@@ -39,4 +38,3 @@ def git_log():
 
     contributor_ref.set(contributor_data)
     return #result
-
