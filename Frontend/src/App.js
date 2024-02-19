ffbf8c20aea2218507b96965620149266d34e44c
Max Pintchouk <maxpintchouk4321@gmail.com>
2024-02-19 11:17:43 -0500

    added Contract to codebase

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 function App() {
   const refreshGitLog = () => {
-    axios.get('http://127.0.0.1:5000/update-git-log')
-      .then(response => {
-        // Handle the response from the server
-        console.log(response.data);
-        alert("Git Log Updated Successfully!");
-      })
-      .catch(error => {
-        // Handle any errors
-        console.error('There was an error!', error);
-        alert("Failed to update Git Log!");
-      });
+    axios.get("http://127.0.0.1:5000/update-git-log").then((response) => {
+      console.log(response.data);
+      alert("Git Log Updated Successfully!");
+    });
   };
 
   return (
