55ac770738c89cb48134e50f9dd88b19de826538
coffeyyy <46061020+coffeyyy@users.noreply.github.com>
2024-02-19 12:30:13 -0500

    add simple git refresh counter

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  // state to keep track of successful git changes
+  const [counter, setCounter] = useState(0);
+
   const refreshGitLog = () => {
-    axios.get('http://127.0.0.1:5000/update-git-log')
-      .then(response => {
+    axios
+      .get("http://127.0.0.1:5000/update-git-log")
+      .then((response) => {
         // Handle the response from the server
         console.log(response.data);
         alert("Git Log Updated Successfully!");
+        // counter is increased by 1 everytime there is a successful git change
+        setCounter((prevCounter) => prevCounter + 1);
       })
-      .catch(error => {
+      .catch((error) => {
         // Handle any errors
-        console.error('There was an error!', error);
+        console.error("There was an error!", error);
         alert("Failed to update Git Log!");
       });
   };
@@ -20,6 +26,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <button onClick={refreshGitLog}>Refresh</button>
+        <h1>Successful Changes: {counter}</h1>
       </header>
     </div>
   );
