c584517abc9f02111e778051ab5794ed08fd5ae5
coffeyyy <46061020+coffeyyy@users.noreply.github.com>
2024-02-19 21:49:07 -0500

    added basic counter

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,32 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
 
 function App() {
-  // state to keep track of successful git changes
-  const [counter, setCounter] = useState(0);
-
   const refreshGitLog = () => {
-    axios
-      .get("http://127.0.0.1:5000/update-git-log")
-      .then((response) => {
-        // Handle the response from the server
-        console.log(response.data);
-        alert("Git Log Updated Successfully!");
-        // counter is increased by 1 everytime there is a successful git change
-        setCounter((prevCounter) => prevCounter + 1);
-      })
-      .catch((error) => {
-        // Handle any errors
-        console.error("There was an error!", error);
-        alert("Failed to update Git Log!");
-      });
+    axios.get("http://127.0.0.1:5000/update-git-log").then((response) => {
+      console.log(response.data);
+      alert("Git Log Updated Successfully!");
+    });
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={refreshGitLog}>Refresh</button>
-        <h1>Successful Changes: {counter}</h1>
       </header>
     </div>
   );
