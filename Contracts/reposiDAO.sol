c7c06a4ea833355a99f5d1920b35741e80047540
Max Pintchouk <maxpintchouk4321@gmail.com>
2024-02-22 16:59:52 -0500

    Contract DAO functions

--- Contracts/reposiDAO.sol
+++ Contracts/reposiDAO.sol
@@ -10,41 +10,59 @@ import "@openzeppelin/contracts@5.0.1/token/ERC20/extensions/ERC20Votes.sol";
 /// @custom:security-contact pintchom@bc.edu
 contract ReposiDAO is ERC20, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
 
-    mapping(address => bool) private _isMinter;
+    struct Proposal {
+        string description;
+        uint256 id;
+        bool executed;
+        uint256 votesFor;
+        uint256 votesAgainst;
+    }
+
+    mapping(uint256 => Proposal) public proposals;
+    uint256 public proposalCount;
+
+    // Track if a token holder has voted on a specific proposal
+    mapping(uint256 => mapping(address => bool)) public hasVoted;
 
     constructor(address initialOwner)
         ERC20("ReposiDAO", "REPO")
         Ownable(initialOwner)
         ERC20Permit("ReposiDAO")
     {
-        transferOwnership(initialOwner);
-        _isMinter[initialOwner] = true;
+        proposalCount = 0;
     }
 
-    modifier onlyMinter() {
-        require(_isMinter[_msgSender()], "Caller is not authorized to mint or transfer");
-        _;
+    // Function to create a new proposal
+    function propose(string memory description) public onlyOwner returns (uint256) {
+        uint256 newProposalId = proposalCount++;
+        proposals[newProposalId] = Proposal({
+            description: description,
+            id: newProposalId,
+            executed: false,
+            votesFor: 0,
+            votesAgainst: 0
+        });
+        return newProposalId;
+    }
+
+    function vote(uint256 proposalId, bool support) public {
+        require(balanceOf(msg.sender) > 0, "No voting tokens held");
+        require(!hasVoted[proposalId][msg.sender], "Already voted");
+        require(proposalId < proposalCount, "Invalid proposal ID");
+
+        Proposal storage proposal = proposals[proposalId];
+
+        if (support) {
+            proposal.votesFor += balanceOf(msg.sender);
+        } else {
+            proposal.votesAgainst += balanceOf(msg.sender);
+        }
+
+        hasVoted[proposalId][msg.sender] = true;
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
-        _isMinter[to] = true;
-    }
-
-    function addMinter(address minter) public onlyOwner {
-        _isMinter[minter] = true;
-    }
-
-    function removeMinter(address minter) public onlyOwner {
-        _isMinter[minter] = false;
-    }
-
-    function transfer(address to, uint256 amount) public override onlyMinter returns (bool) {
-        return super.transfer(to, amount);
-    }
-
-    function transferFrom(address from, address to, uint256 amount) public override onlyMinter returns (bool) {
-        return super.transferFrom(from, to, amount);
     }
 
     // The following functions are overrides required by Solidity.
